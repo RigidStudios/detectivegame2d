b1229aa941e22eb7d7ac1c87680f00f9b3f4114e
olddrives <pulsviorel@gmail.com>
2026-05-15 19:57:01 +0100

    Window platform locking + death animation + other fixes blah blah blah

--- Assets/scriptsDesktop/Window.cs
+++ Assets/scriptsDesktop/Window.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class Window : MonoBehaviour
 {
-    [Header("Window")]
+    [Header("Window + Dragging")]
     public string windowTitle = "App";
     public bool startOpen = false;
-
-    [Header("Dragging")]
     public Transform titleBar;
     public Vector2 dragOffset;
-
-    [Header("Entry Colliders")]
-    public Collider2D entryLeft;
-    public Collider2D entryRight;
-    public Collider2D entryBottom;
 
     [Header("Open/Close Animation")]
     public float openSpeed = 12f;
     public Vector3 closedScale = new Vector3(1f, 0f, 1f); 
     public Vector3 openScale = Vector3.one;
 
+    [Header("Platform Snapping")]
+    public float snapRange = 1.5f;
+    public LayerMask platformLayer;
+    public float windowHalfHeight = 1f;
+
+    [Header("9-Slice Scaling")]
+    public SpriteRenderer windowBackground;
+    public float lockedWidth = 5f;
+    public float defaultWidth = 2f;
+    public float scaleSpeed = 8f;
+
+    [Header("Interactability")]
+    public Collider2D titleBarCollider;
+    public Collider2D closeButtonCollider;
+    public string playerTag = "Player";
+
+    [Header("Adjust child Width")]
+    public Transform[] leftAnchored; 
+    public Transform[] rightAnchored;
+    public Transform[] centreAnchored;
+    public BoxCollider2D rootCollider;
+
+
     //priv 
     bool isOpen = false;
     bool isDragging = false;
+    bool isLocked = false;
+    bool isPlayerInside = false;
+    float[] leftOffsets;
+    float[] rightOffsets;
+    float[] centreWidths;
+    float targetWidth;
     Camera mainCam;
     Vector3 targetScale;
+
+
     
     public bool IsOpen => isOpen;
 
@@ -41,11 +65,19 @@ public class Window : MonoBehaviour
 
         if (startOpen) isOpen = true;
     }
+    void Start()
+    {
+        CacheAnchorOffsets();
+        targetWidth = defaultWidth;
+        if (windowBackground != null)
+            windowBackground.size = new Vector2(defaultWidth, windowBackground.size.y);
+    }
 
     void Update()
     {
         AnimateScale();
         HandleTitleBarDrag();
+        UpdateWindowScale();
     }
 
     public void Open()
@@ -53,14 +85,12 @@ public class Window : MonoBehaviour
         gameObject.SetActive(true);
         isOpen = true;
         targetScale = openScale;
-        SetEntryCollidersActive(true);
     }
 
     public void Close()
     {
         isOpen = false;
         targetScale = closedScale;
-        SetEntryCollidersActive(false);
     }
 
     void AnimateScale()
@@ -73,6 +103,23 @@ public class Window : MonoBehaviour
 
         if (!isOpen && Vector3.Distance(transform.localScale, closedScale) < 0.01f)
             gameObject.SetActive(false);
+    }
+    void UpdateWindowScale()
+    {
+        if (windowBackground == null) return;
+
+        float currentWidth = windowBackground.size.x;
+        float newWidth = Mathf.Lerp(currentWidth, targetWidth, Time.deltaTime * scaleSpeed);
+        windowBackground.size = new Vector2(newWidth, windowBackground.size.y);
+
+        UpdateChildAnchors(newWidth);
+
+        if (rootCollider != null)
+        {
+            Vector2 size = rootCollider.size;
+            size.x = newWidth;
+            rootCollider.size = size;
+        }
     }
 
     void HandleTitleBarDrag()
@@ -95,12 +142,105 @@ public class Window : MonoBehaviour
     public void OnTitleBarMouseUp()
     {
         isDragging = false;
+        SnapToPlatform();
+    }
+    void SnapToPlatform()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            Vector2.down,
+            snapRange,
+            platformLayer
+        );
+
+        if (hit.collider != null)
+        {
+            Vector3 snapped = transform.position;
+            snapped.y = hit.point.y + windowHalfHeight;
+            transform.position = snapped;
+            isLocked = true;
+            targetWidth = lockedWidth;
+        }
+        else
+        {
+            isLocked = false;
+            targetWidth = defaultWidth;
+        }
+    }
+    void CacheAnchorOffsets()
+    {
+        leftOffsets = new float[leftAnchored.Length];
+        rightOffsets = new float[rightAnchored.Length];
+        centreWidths = new float[centreAnchored.Length];
+
+        for (int i = 0; i < leftAnchored.Length; i++)
+            leftOffsets[i] = leftAnchored[i].localPosition.x + (defaultWidth * 0.5f);
+
+        for (int i = 0; i < rightAnchored.Length; i++)
+            rightOffsets[i] = rightAnchored[i].localPosition.x - (defaultWidth * 0.5f);
+
+        for (int i = 0; i < centreAnchored.Length; i++)
+        {
+            BoxCollider2D bc = centreAnchored[i].GetComponent<BoxCollider2D>();
+            centreWidths[i] = bc != null ? bc.size.x / defaultWidth : 1f;
+        }
     }
 
-    void SetEntryCollidersActive(bool active)
+    void UpdateChildAnchors(float currentWidth)
     {
-        if (entryLeft) entryLeft.enabled = active;
-        if (entryRight) entryRight.enabled = active;
-        if (entryBottom) entryBottom.enabled = active;
+        float halfWidth = currentWidth * 0.5f;
+
+        for (int i = 0; i < leftAnchored.Length; i++)
+        {
+            Vector3 p = leftAnchored[i].localPosition;
+            p.x = -halfWidth + leftOffsets[i];
+            leftAnchored[i].localPosition = p;
+        }
+        for (int i = 0; i < rightAnchored.Length; i++)
+        {
+            Vector3 p = rightAnchored[i].localPosition;
+            p.x = halfWidth + rightOffsets[i];
+            rightAnchored[i].localPosition = p;
+        }
+
+        for (int i = 0; i < centreAnchored.Length; i++)
+        {
+            Vector3 p = centreAnchored[i].localPosition;
+            p.x = 0f;
+            centreAnchored[i].localPosition = p;
+
+            BoxCollider2D bc = centreAnchored[i].GetComponent<BoxCollider2D>();
+            if (bc != null)
+            {
+                Vector2 size = bc.size;
+                size.x = currentWidth * centreWidths[i];
+                bc.size = size;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerInside = true;
+            SetInteractable(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            isPlayerInside = false;
+            SetInteractable(true);
+        }
+    }
+
+    void SetInteractable(bool interactable)
+    {
+        if (titleBarCollider != null) titleBarCollider.enabled = interactable;
+        if (closeButtonCollider != null) closeButtonCollider.enabled = true;
+        if (!interactable) isDragging = false;
     }
 }
